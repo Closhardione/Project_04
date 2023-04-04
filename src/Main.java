83225477d4961c3634db7b7e125993f8b2be940c
Student <student@umcs.pl>
2023-04-04 15:40:43 +0200

    zad2

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
         CustomList<String> test1 = new CustomList<>();
         test1.addFirst("1");
         test1.addLast("2");
-        System.out.println(test1.getFirst()+" "+ test1.getLast());
+        System.out.println(test1.getFirst() + " " + test1.getLast());
     }
+}
